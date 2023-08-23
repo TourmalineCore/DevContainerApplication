5989338ac185bda4a7dbaf148b037e2ea56e85eb
Olga Kuzmicheva <okuzmicheva@tourmalinecore.com>
2023-08-23 14:42:12 +0500

    feat(test): simplify test

--- tests/example_unit_test.cpp
+++ tests/example_unit_test.cpp
@@ -1,13 +1,6 @@
 #include <gtest/gtest.h>
-#include <cmath>
 
 TEST(Math, TestCMath)
 {
-    double param, result;
-    param = 60.0;
-    result = cos ( param * 3.14 / 180.0 );
-
-    double ideal_result = 1.5;
-
     EXPECT_EQ(0, 0);
 }
