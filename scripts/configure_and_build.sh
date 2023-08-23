2c70b2bf1f69a0239c408c47cc2f1cc71cbe85ef
Olga Kuzmicheva <okuzmicheva@tourmalinecore.com>
2023-08-23 16:42:53 +0500

    ci(test): add building stage

---
+++ scripts/configure_and_build.sh
@@ -0,0 +1,6 @@
+mkdir cmake-build
+cd cmake-build
+
+cmake .. && cmake --build ./ --target all --config "Debug"
+
+cd ..
--- scripts/run_unit_tests.sh
+++ scripts/run_unit_tests.sh
@@ -1,3 +1,3 @@
 echo "Testing Project"
-( cd ./build || exit 1
+( cd ./cmake-build || exit 1
 ctest -j"$(nproc)" -C ${CMAKE_BUILD_TYPE:-Debug} -T test --output-on-failure)
